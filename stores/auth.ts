5da081640d511ed36d3a1c89bc67e7177195488f
Upperstrange <94213158+Upperstrange@users.noreply.github.com>
2025-05-31 22:31:16 +0530

    :sparkles: added SSR support for better auth and changed middleware to not get-session client side each time (#58)

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,9 +1,15 @@
 import { authClient } from "~/lib/auth-client";
 
 export const useAuthStore = defineStore("authStore", () => {
-  const session = authClient.useSession();
-  const user = computed(() => session.value.data?.user);
-  const loading = computed(() => session.value.isPending || session.value.isRefetching);
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+
+  // for SSR, we are using the useFetch composable to fetch the session. init function will run when we first load the app and the session would be available throughout
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -23,5 +29,6 @@ export const useAuthStore = defineStore("authStore", () => {
     signIn,
     signOut,
     user,
+    init,
   };
 });
