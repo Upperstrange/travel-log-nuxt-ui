2f22911f23e42b46cc89f9eec3df95a3107dc4dc
Upperstrange <94213158+Upperstrange@users.noreply.github.com>
2025-06-21 00:03:21 +0530

    Update header.vue added img logo instead of the component

--- components/app/header.vue
+++ components/app/header.vue
@@ -15,7 +15,7 @@ const navLinks = [
       <div class="flex justify-between items-center h-16">
         <!-- Logo -->
         <NuxtLink to="/" class="flex items-center">
-          <Logo />
+          <NuxtImg src="/logo.png"/>
           <span class="ml-2 text-2xl font-bold font-logo text-neutral-900 dark:text-white">Let'sGo</span>
         </NuxtLink>
         <!-- Desktop Menu -->
